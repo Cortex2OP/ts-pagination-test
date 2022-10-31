0ad2645d5018138e89d04b6cba5d488003c9ecc8
Sanjo <sanjin.pajic@corussoft.de>
2022-10-31 14:54:56 +0100

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import axios from 'axios';
-import Image from './components/Image/Image';
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import axios from "axios";
+import Image from "./components/Image/Image";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import "./testing.css";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface ITest {
-  author: string,
-  download_url: string,
-  height: number,
-  id: number,
-  url: string,
-  width: number
+  author: string;
+  download_url: string;
+  height: number;
+  id: number;
+  url: string;
+  width: number;
 }
+
 
 const StyledApp = styled.div({
   display: "flex",
@@ -19,23 +21,32 @@ const StyledApp = styled.div({
   margin: "20px 5% 0 5%",
   justifyContent: "left",
   width: "90vw",
-})
+});
 
 function App() {
   const [photoData, setPhotoData] = useState<ITest[]>([]);
-  
-  useEffect( () => {
-    axios.get("https://picsum.photos/v2/list?page=2&limit=30").then(res => setPhotoData(res.data));
-  }, [])
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    axios.get(`https://picsum.photos/v2/list?page=${page}&limit=15`).then((res) => setPhotoData([...photoData, ...res.data]));
+  }, [page]);
 
   return (
     <StyledApp className="App">
-      {photoData.map(photo => 
-      <Image 
-      key = {photo.id} 
-      url = {photo.download_url} 
-      />)}
-      </StyledApp>
+      <InfiniteScroll
+      className="infinite-scroll"
+        dataLength={photoData.length}
+        loader={<h4>Loading...</h4>}
+        hasMore={true}
+        next={() => {
+          setPage(page + 1);
+        }}
+      >
+        {photoData.map((photo) => (
+          <Image key={photo.id} url={photo.download_url}></Image>
+        ))}
+      </InfiniteScroll>
+    </StyledApp>
   );
 }
 
